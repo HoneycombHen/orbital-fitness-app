16fec58bb113b0cf423892116b63b0d55ea39fc7
Hendrick <hendricklim8685@gmail.com>
2026-05-28 23:13:41 +0800

    Implement AsyncStorage persistence for saved workouts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,65 @@
-import { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 import WorkoutCard from '../../components/workoutCard';
 
 
-
 export default function HomeScreen() {
 
+  // states section
   const [workout, setWorkout] = useState('');
   const [sets, setSets] = useState('');
   const [reps, setReps] = useState('');
   const [savedWorkouts, setSavedWorkouts] =
   useState<{ name: string; sets: string; reps: string }[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  
+  // calls saveWorkouts function whenever change to savedWorkouts state
+  useEffect(() => {
+    if (isLoaded) {
+      saveWorkouts();
+    }
+  }, 
+  [savedWorkouts, isLoaded]);
+
+  // saveWorkouts function: to save workouts to AsyncStorage upon every change to savedWorkouts state
+  const saveWorkouts = async () => {
+    try {
+      await AsyncStorage.setItem(
+        'workouts',
+        JSON.stringify(savedWorkouts)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // calls loadWorkouts function on component mount to load any saved workouts from AsyncStorage
+  useEffect(() => {
+    loadWorkouts();
+  }, 
+  []);
+
+  const loadWorkouts = async () => {
+  try {
+    const storedWorkouts =
+      await AsyncStorage.getItem('workouts');
+
+    if (storedWorkouts) {
+      setSavedWorkouts(
+        JSON.parse(storedWorkouts)
+      );
+
+      setIsLoaded(true);
+      
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+  // deleteWorkout function: to delete a workout from the savedWorkouts state
   const deleteWorkout = (indexToDelete: number) => {
     setSavedWorkouts(
       savedWorkouts.filter(
@@ -19,6 +67,8 @@ export default function HomeScreen() {
       )
     );
   };
+
+  
 
   return (
     <View
@@ -76,7 +126,6 @@ export default function HomeScreen() {
         }}
       />
       
-
       <Button
         title="Save Workout"
         onPress={() => setSavedWorkouts([...savedWorkouts,
